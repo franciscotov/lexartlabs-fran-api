9efc018a573084ee94037d9524fd4d1b3f88986a
franciscotov <franciscodejesus.tovarceballo.contractor@gdevtools.com>
2024-10-16 15:00:07 -0300

    deleting authenticationManager v2

--- src/main/java/com/lexartlabs/fran/api/controller/AuthController.java
+++ src/main/java/com/lexartlabs/fran/api/controller/AuthController.java
@@ -3,13 +3,12 @@ package com.lexartlabs.fran.api.controller;
 import com.lexartlabs.fran.api.config.auth.TokenProvider;
 import com.lexartlabs.fran.api.dto.SignInDataDto;
 import com.lexartlabs.fran.api.dto.SignUpDataDto;
-import com.lexartlabs.fran.api.entities.User;
 import com.lexartlabs.fran.api.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
